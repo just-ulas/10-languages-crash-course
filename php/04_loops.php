cddda90eabdfea9db5e6de2a9019c59696b9de12
just-ulas <ulasy6337@gmail.com>
2026-08-25 21:21:28 +0300

    php 04 loops

---
+++ php/04_loops.php
@@ -0,0 +1,15 @@
+<?php
+for ($i = 0; $i < 5; $i++) {
+    echo $i . "\n";
+}
+
+$fruits = ["apple", "banana", "cherry"];
+foreach ($fruits as $fruit) {
+    echo $fruit . "\n";
+}
+
+$count = 0;
+while ($count < 3) {
+    echo "count is $count\n";
+    $count++;
+}
