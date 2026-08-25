44de449b6401c04ade9f228bd8a30e610ded2b91
just-ulas <ulasy6337@gmail.com>
2026-08-25 20:52:29 +0300

    python 04 loops

---
+++ python/04_loops.py
@@ -0,0 +1,16 @@
+# 04 loops
+
+# for loop with range
+for i in range(5):
+    print(i)
+
+# for over list
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+# while loop
+count = 0
+while count < 3:
+    print("count is", count)
+    count += 1
