c78fe23cf9b01d7328bc1cefd3f2496fdcffde91
just-ulas <ulasy6337@gmail.com>
2026-08-25 21:42:24 +0300

    rust 13 modules

---
+++ rust/13_modules.rs
@@ -0,0 +1,9 @@
+use std::f64::consts::PI;
+
+fn main() {
+    println!("{}", 16f64.sqrt());
+    println!("{}", PI);
+
+    // random needs external crate usually, keep simple
+    println!("{}", "use rand crate for random numbers");
+}
