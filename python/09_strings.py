f4fd176b0dfeeb32fb064966de3a839559e7b6f6
just-ulas <ulasy6337@gmail.com>
2026-08-25 21:28:06 +0300

    python 09 strings

---
+++ python/09_strings.py
@@ -0,0 +1,19 @@
+# 09 strings
+
+name = "ulas"
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(len(name))
+
+# f-string
+age = 20
+print(f"{name} is {age} years old")
+
+# slicing
+print(name[0:2])   # ul
+print(name[::-1])  # salu (reverse)
+
+# check
+print("ula" in name)
+print(name.startswith("u"))
