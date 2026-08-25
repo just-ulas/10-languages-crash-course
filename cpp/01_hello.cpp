aa03af165516a0ef2ffa1c9129c1c441c92e5209
just-ulas <ulasy6337@gmail.com>
2026-08-25 21:20:51 +0300

    cpp 01 hello

---
+++ cpp/01_hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello from c++" << endl;
+
+    string name = "ulas";
+    cout << "hi " << name << endl;
+
+    return 0;
+}
