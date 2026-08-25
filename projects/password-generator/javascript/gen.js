24725c442678c9184080c03c4e4e4a788b862ee4
just-ulas <ulasy6337@gmail.com>
2026-08-25 23:17:53 +0300

    password gen js: crypto.randomBytes CSPRNG

--- projects/password-generator/javascript/gen.js
+++ projects/password-generator/javascript/gen.js
@@ -1,8 +1,13 @@
+// Password generator using crypto (CSPRNG), not Math.random
+
+const crypto = require("crypto");
+
 const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
 const length = 12;
 
+const bytes = crypto.randomBytes(length);
 let password = "";
 for (let i = 0; i < length; i++) {
-  password += chars[Math.floor(Math.random() * chars.length)];
+  password += chars[bytes[i] % chars.length];
 }
 console.log("Password:", password);
