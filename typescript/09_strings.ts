5bda5091706b6a41ba51e67a5d4d3d8a6c6d9346
just-ulas <ulasy6337@gmail.com>
2026-08-25 23:30:02 +0300

    fix ts 09: name clash with DOM

--- typescript/09_strings.ts
+++ typescript/09_strings.ts
@@ -1,15 +1,16 @@
 // 09 strings
+export {};
 
-const name: string = "ulas";
-console.log(name.toUpperCase());
-console.log(name.toLowerCase());
-console.log(name.length);
+const personName: string = "ulas";
+console.log(personName.toUpperCase());
+console.log(personName.toLowerCase());
+console.log(personName.length);
 
 const age: number = 20;
-console.log(`${name} is ${age} years old`);
+console.log(`${personName} is ${age} years old`);
 
-console.log(name.slice(0, 2)); // ul
-console.log(name.split("").reverse().join(""));
+console.log(personName.slice(0, 2)); // ul
+console.log(personName.split("").reverse().join(""));
 
-console.log(name.includes("ula"));
-console.log(name.startsWith("u"));
+console.log(personName.includes("ula"));
+console.log(personName.startsWith("u"));
