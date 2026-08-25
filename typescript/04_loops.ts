c4d1a4136c488613842699d92018324d2cf59a6e
just-ulas <ulasy6337@gmail.com>
2026-08-25 21:20:03 +0300

    ts 04 loops

---
+++ typescript/04_loops.ts
@@ -0,0 +1,14 @@
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+const fruits: string[] = ["apple", "banana", "cherry"];
+for (const fruit of fruits) {
+  console.log(fruit);
+}
+
+let count = 0;
+while (count < 3) {
+  console.log("count is", count);
+  count++;
+}
