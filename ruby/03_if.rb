0b9d81e8d9a7b872611697a9aa16479f30f5ce4e
just-ulas <ulasy6337@gmail.com>
2026-08-25 21:20:27 +0300

    ruby 03 if

---
+++ ruby/03_if.rb
@@ -0,0 +1,19 @@
+age = 20
+
+if age >= 18
+  puts "adult"
+else
+  puts "not adult yet"
+end
+
+score = 85
+
+if score >= 90
+  puts "A"
+elsif score >= 80
+  puts "B"
+elsif score >= 70
+  puts "C"
+else
+  puts "need more work"
+end
