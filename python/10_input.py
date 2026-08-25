4140ae89a8e4706f7ec9557ddca2a6e082f872f2
just-ulas <ulasy6337@gmail.com>
2026-08-25 21:29:43 +0300

    python 10 input

---
+++ python/10_input.py
@@ -0,0 +1,11 @@
+# 10 input
+
+name = input("What is your name? ")
+print(f"Hello {name}!")
+
+age = input("How old are you? ")
+print(f"You are {age} years old.")
+
+# convert to int
+age_num = int(age)
+print(f"Next year you will be {age_num + 1}")
