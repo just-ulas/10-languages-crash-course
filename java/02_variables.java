86a0e46bef4bc57902ab76c7fb04f62dc67deefd
just-ulas <ulasy6337@gmail.com>
2026-08-25 21:19:29 +0300

    java 02 variables

---
+++ java/02_variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        String name = "ulas";
+        int age = 20;
+        double height = 1.75;
+        boolean isStudent = true;
+
+        System.out.println(name + " " + age + " " + height + " " + isStudent);
+
+        age = 21;
+        System.out.println("new age: " + age);
+    }
+}
