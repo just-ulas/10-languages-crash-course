d215797815f7438ac6c55f04b0d3dbbb03544b21
just-ulas <ulasy6337@gmail.com>
2026-08-25 22:11:09 +0300

    rust 19 packages

---
+++ rust/19_packages.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // Rust packages:
+    //   cargo new myapp
+    //   cargo add serde
+    //   Cargo.toml + Cargo.lock
+
+    println!("Rust packaging:");
+    println!("  Cargo.toml + Cargo.lock");
+    println!("  cargo add / cargo build");
+    println!("  use package::item;");
+}
