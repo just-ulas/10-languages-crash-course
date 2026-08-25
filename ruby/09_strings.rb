9155aa092ed69f9cca5663b96648d59bf88038e0
just-ulas <ulasy6337@gmail.com>
2026-08-25 21:29:25 +0300

    ruby 09 strings

---
+++ ruby/09_strings.rb
@@ -0,0 +1,15 @@
+# 09 strings
+
+name = "ulas"
+puts name.upcase
+puts name.downcase
+puts name.length
+
+age = 20
+puts "#{name} is #{age} years old"
+
+puts name[0, 2]      # ul
+puts name.reverse
+
+puts name.include?("ula")
+puts name.start_with?("u")
