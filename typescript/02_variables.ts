8a592fa4d03afb299771f53bdf7cb4a725a0469b
just-ulas <ulasy6337@gmail.com>
2026-08-25 21:19:58 +0300

    ts 02 variables

---
+++ typescript/02_variables.ts
@@ -0,0 +1,11 @@
+let name: string = "ulas";
+let age: number = 20;
+let height: number = 1.75;
+let isStudent: boolean = true;
+
+console.log(name, age, height, isStudent);
+
+age = 21;
+console.log("new age:", age);
+
+const pi: number = 3.14;
