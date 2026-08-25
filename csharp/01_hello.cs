1e1f932d270827f06bb7a8ca516c095376f3f956
just-ulas <ulasy6337@gmail.com>
2026-08-25 21:22:31 +0300

    csharp 01 hello

---
+++ csharp/01_hello.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("hello from csharp");
+
+        string name = "ulas";
+        Console.WriteLine("hi " + name);
+    }
+}
