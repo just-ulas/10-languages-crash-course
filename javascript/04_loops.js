642dc5b3da46515ffcd116cb02f23629caa9320d
just-ulas <ulasy6337@gmail.com>
2026-08-25 20:52:59 +0300

    js 04 loops

---
+++ javascript/04_loops.js
@@ -0,0 +1,19 @@
+// 04 loops
+
+// for loop
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+// for of
+const fruits = ["apple", "banana", "cherry"];
+for (const fruit of fruits) {
+  console.log(fruit);
+}
+
+// while
+let count = 0;
+while (count < 3) {
+  console.log("count is", count);
+  count++;
+}
