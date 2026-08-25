e404c1bcdf328ea6c120bb84784981eb226da0fa
just-ulas <ulasy6337@gmail.com>
2026-08-25 23:18:28 +0300

    password gen go: crypto/rand CSPRNG

--- projects/password-generator/go/gen.go
+++ projects/password-generator/go/gen.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
-	"math/rand"
-	"time"
+	"math/big"
 )
 
 func main() {
 	chars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%"
 	length := 12
 
-	rand.Seed(time.Now().UnixNano())
 	password := make([]byte, length)
 	for i := range password {
-		password[i] = chars[rand.Intn(len(chars))]
+		n, err := rand.Int(rand.Reader, big.NewInt(int64(len(chars))))
+		if err != nil {
+			panic(err)
+		}
+		password[i] = chars[n.Int64()]
 	}
 	fmt.Println("Password:", string(password))
 }
