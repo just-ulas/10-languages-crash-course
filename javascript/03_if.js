765182e13091681e0f89f36ff409cd502d0cba77
just-ulas <ulasy6337@gmail.com>
2026-08-25 20:52:56 +0300

    js 03 if

---
+++ javascript/03_if.js
@@ -0,0 +1,22 @@
+// 03 if / else
+
+const age = 20;
+
+if (age >= 18) {
+  console.log("adult");
+} else {
+  console.log("not adult yet");
+}
+
+// else if
+const score = 85;
+
+if (score >= 90) {
+  console.log("A");
+} else if (score >= 80) {
+  console.log("B");
+} else if (score >= 70) {
+  console.log("C");
+} else {
+  console.log("need more work");
+}
