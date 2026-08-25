6f2ae8d44c7faca3b2b2c60e995b1a254386ec22
just-ulas <ulasy6337@gmail.com>
2026-08-25 21:20:29 +0300

    ruby 04 loops

---
+++ ruby/04_loops.rb
@@ -0,0 +1,14 @@
+5.times do |i|
+  puts i
+end
+
+fruits = ["apple", "banana", "cherry"]
+fruits.each do |fruit|
+  puts fruit
+end
+
+count = 0
+while count < 3
+  puts "count is #{count}"
+  count += 1
+end
