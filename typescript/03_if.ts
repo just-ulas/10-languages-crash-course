2bb2e65c0fd9b8978db60d4b478e88dde0101f26
just-ulas <ulasy6337@gmail.com>
2026-08-25 21:20:01 +0300

    ts 03 if

---
+++ typescript/03_if.ts
@@ -0,0 +1,19 @@
+const age: number = 20;
+
+if (age >= 18) {
+  console.log("adult");
+} else {
+  console.log("not adult yet");
+}
+
+const score: number = 85;
+
+if (score >= 90) {
+  console.log("A");
+} else if (score >= 80) {
+  console.log("B");
+} else if (score >= 70) {
+  console.log("C");
+} else {
+  console.log("need more work");
+}
