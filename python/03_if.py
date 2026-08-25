ca392a6e4ae72d663c11ab611a323430743a3f2f
just-ulas <ulasy6337@gmail.com>
2026-08-25 20:52:27 +0300

    python 03 if

---
+++ python/03_if.py
@@ -0,0 +1,20 @@
+# 03 if / else
+
+age = 20
+
+if age >= 18:
+    print("adult")
+else:
+    print("not adult yet")
+
+# elif example
+score = 85
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+else:
+    print("need more work")
